d11f64e26fe262754dc16d7c9d39c145245bcb5c
Joe Smith <yasumoto7@gmail.com>
2009-05-26 14:24:36 -0700

    Fone ftw. Signed-off-by: Joe Smith <yasumoto7@gmail.com>

---
+++ presentation/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+pdflatex joeSmith_Presentation_Classes.tex &&  pdflatex joeSmith_Presentation_Classes.tex && evince joeSmith_Presentation_Classes.pdf
--- triangle.sh
+++ triangle.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-make clean && make && cd cls && java Triangle.Compiler ../joe.tri
+make clean && make && cd cls && echo "java Triangle.Compiler ../joe.tri" && java Triangle.Compiler ../joe.tri
 
+echo "java TAM.Intepreter obj.tam\n"
 java TAM.Interpreter obj.tam
